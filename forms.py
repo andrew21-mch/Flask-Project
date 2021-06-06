6d0202fc17f09d522ed6131e0b8e2a61ca3e9851
andrew21-mch <nfonandrew73@gmail.com>
2021-06-06 16:50:03 +0100

    created forms.py to take care of login and registration

---
+++ forms.py
@@ -0,0 +1,21 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms import PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+#registration form
+class RegistrationForm(FlaskForm):
+    username = StringField('username',validators=[DataRequired(), Length(min=4, max=20)])
+    email = StringField('email',validators=[DataRequired(), Email()])
+    password = PasswordField('password',validators=[DataRequired(), Length(min=6, max=12)])
+    password_repeat = PasswordField('repeat password',validators=[DataRequired(), Length(min=6, max=12),EqualTo(password)])
+    submit = SubmitField('signup')
+
+#login form
+class LoginForm(FlaskForm):
+    email = StringField('email',validators=[DataRequired(), Email()])
+    password = PasswordField('password',validators=[DataRequired()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Login')
+    
+
